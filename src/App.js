07b1955a3fccaac86b146d6ac0e9136829275f11
Nadiia Chorna <nadiia.chorna@epam.com>
2024-05-27 14:54:34 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react'
+
+function App() {
+  const [dateOfReviewInputValue, setDateOfReviewInputValue] = useState({
+    id: 'dateOfReview',
+    label: `Date Of Review`,
+    value: '',
+    isError: false,
+    isRequired: true,
+    editable: true,
+  })
+  const setTextDataFormValue = (key, selectedValue) => {
+    const dateFormatted = selectedValue ? new Date(selectedValue).toISOString() : ''
+    setDateOfReviewInputValue({ ...dateOfReviewInputValue, value: dateFormatted })
+  }
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <div
+              className={`nl-intake-initiative-details__text-input-label-wrapper`}>
+            <label className={`nl-custom-text-area__label`} htmlFor={'dateOfReview'}>
+              {`Date Of Review`}
+            </label>
+            <input
+                className={`nl-intake-initiative-details__date-review-input`}
+                type={'date'}
+                id={'dateOfReview'}
+                name={'dateOfReview'}
+                max={'9999-12-31'}
+                min={() => new Date().toISOString().split('T')[0]}
+                value={dateOfReviewInputValue?.value ? dateOfReviewInputValue.value.split('T')[0] : ''}
+                onChange={e => {
+                  // Necessary for correct operation of the Reset button on iOS
+                  e.target.defaultValue = ''
+                  setTextDataFormValue('dateOfReview', e.target.value)
+                }}
+            />
+          </div>
+        </header>
+      </div>
+  );
+}
+
+export default App;
